25fd2cfa919a16a9ac952f2dc8f0e93295f762dd
Ming Li <Mingrkli@gmail.com>
2023-02-13 14:10:35 -0800

    update
    added a new project I did

--- data/projects.js
+++ data/projects.js
@@ -1,82 +1,87 @@
 projects = [
     {
-        title: 'Eyeballs Watching',
-        img: 'img/other-projects/eye.png',
+        title: "Realtime Chatroom",
+        img: "img/other-projects/rtc.png",
         popup: {
-            link: 'https://mingrkli-spooky.netlify.app/',
-            subDesc: "Eyeballs staring at your mouse while moving away of you get too close.",
-        }
+            link: "https://chat-app-d860e.web.app/",
+            subDesc: "A realtime chatroom with google authentication",
+        },
     },
     {
-        title: 'Sound Hearing Animation',
-        img: 'img/other-projects/sa.png',
+        title: "Eyeballs Watching",
+        img: "img/other-projects/eye.png",
         popup: {
-            link: 'https://sound-hearing-animation.netlify.app/',
-            subDesc: "Choose a sound file and watch the animations go with colorful lights.",
-        }
+            link: "https://mingrkli-spooky.netlify.app/",
+            subDesc:
+                "Eyeballs staring at your mouse while moving away of you get too close.",
+        },
     },
     {
-        title: 'Shape Drawing Tool',
-        img: 'img/other-projects/dt.png',
+        title: "Sound Hearing Animation",
+        img: "img/other-projects/sa.png",
         popup: {
-            link: 'https://shape-drawing-tool.netlify.app/',
+            link: "https://sound-hearing-animation.netlify.app/",
+            subDesc:
+                "Choose a sound file and watch the animations go with colorful lights.",
+        },
+    },
+    {
+        title: "Shape Drawing Tool",
+        img: "img/other-projects/dt.png",
+        popup: {
+            link: "https://shape-drawing-tool.netlify.app/",
             subDesc: "A drawing tool but with shapes!",
-        }
+        },
     },
     {
-        title: 'Shape Creator Random',
-        img: 'img/other-projects/sr.png',
+        title: "Shape Creator Random",
+        img: "img/other-projects/sr.png",
         popup: {
-            link: 'https://cool-shapes.netlify.app/',
+            link: "https://cool-shapes.netlify.app/",
             subDesc: "Randomized and customizable shapes.",
-        }
+        },
     },
     {
-        title: 'Mouse Character Trail',
-        img: 'img/other-projects/ct.png',
+        title: "Mouse Character Trail",
+        img: "img/other-projects/ct.png",
         popup: {
-            link: 'https://character-trail.netlify.app/',
-            subDesc: "Move your mouse to see a colorful RGB trail of random letters, numbers, and japanese characters.",
-        }
+            link: "https://character-trail.netlify.app/",
+            subDesc:
+                "Move your mouse to see a colorful RGB trail of random letters, numbers, and japanese characters.",
+        },
     },
     {
-        title: 'Minnew News Site - Suggested by Jake',
-        img: 'img/other-projects/minnew.png',
+        title: "Minnew News Site - Suggested by Jake",
+        img: "img/other-projects/minnew.png",
         popup: {
-            link: 'https://mingrkli-minnew.netlify.app/',
+            link: "https://mingrkli-minnew.netlify.app/",
             subDesc: "A cool 3d card feel news site",
-        }
+        },
     },
     {
         title: "Jake's Best Day",
-        img: 'img/other-projects/yoi.png',
+        img: "img/other-projects/yoi.png",
         popup: {
-            link: 'https://jake-best-day.netlify.app/',
-            subDesc: "A site created for a buddy of mine as it's his best day ever.",
-        }
+            link: "https://jake-best-day.netlify.app/",
+            subDesc:
+                "A site created for a buddy of mine as it's his best day ever.",
+        },
     },
     {
-        title: 'Mesco Shopping Site With Working Cart',
-        img: 'img/other-projects/mesco.png',
+        title: "Mesco Shopping Site With Working Cart",
+        img: "img/other-projects/mesco.png",
         popup: {
-            link: 'https://mesco-shopping.netlify.app/',
+            link: "https://mesco-shopping.netlify.app/",
             subDesc: "A shopping site with working carts and filter.",
-        }
+        },
     },
     {
         title: "Peakalo's Diner - Suggested by Ace",
-        img: 'img/other-projects/diner.png',
+        img: "img/other-projects/diner.png",
         popup: {
-            link: 'https://peakalos-diner.netlify.app/',
-            subDesc: "A restaurant with a menu when hover over names, shows the food image.",
-        }
+            link: "https://peakalos-diner.netlify.app/",
+            subDesc:
+                "A restaurant with a menu when hover over names, shows the food image.",
+        },
     },
-    {
-        title: 'Color Time With Local Storage',
-        img: 'img/other-projects/cc.png',
-        popup: {
-            link: 'https://mingrkli-color-time.netlify.app/',
-            subDesc: "A cool clock in which you can edit the colors.",
-        }
-    },
-]
+];
